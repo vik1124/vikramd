ccf9a8a71d4e4d0e0944c4126af38286434b9ee1
Vikram Divakar <vik1124@gmail.com>
2017-03-21 05:31:37 +0000

    adding basic information

--- templates/HomeController.js
+++ templates/HomeController.js
@@ -2,4 +2,16 @@ var app = angular.module('Home',[]);
 
 app.controller('HomeController', function($scope, $window, $http, $sce) {
     $scope.tabTitle = $sce.trustAsHtml("Resum\u00E9");
+    $scope.homeShow = true;
+    $scope.projectsShow = false;
+
+    $scope.gotoHome = function(){
+        $scope.homeShow = true;
+        $scope.projectsShow = false;
+    };
+
+    $scope.gotoProjects = function(){
+        $scope.homeShow = false;
+        $scope.projectsShow = true;
+    }
 });
